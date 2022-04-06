c5cc9d2bfe575ff380f49d748e28612dcc926dcf
Welisson luca <welissonluca17@gmail.com>
2022-04-06 08:03:47 -0300

    test: should try to validate an cpf if all digits the same

--- test/CPF.test.ts
+++ test/CPF.test.ts
@@ -1,11 +1,16 @@
 import { CPF } from "../src/Cpf";
 
-test("should return valid CPF", function () {
+test("should return valid CPF", () => {
   const cpf = new CPF("152.726.480-72");
 	expect(cpf).toBeTruthy();
 });
-test("should try to validate an invalid cpf", function () {
-	 const cpf = new CPF("111.111.111-11");
+test("should try to validate an invalid cpf", () => {
+	const cpf = new CPF("213.544.111-11");
+	expect(cpf).toBeFalsy();
+});
+
+test("should try to validate an cpf if all digits the same", () => {
+	const cpf = new CPF("111.111.111-11");
 	expect(cpf).toBeFalsy();
 });
 
