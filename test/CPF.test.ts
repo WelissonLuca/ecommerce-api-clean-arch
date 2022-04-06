a58baef737f5d0ee0b301294fe65d07309298c95
Welisson luca <welissonluca17@gmail.com>
2022-04-06 08:11:37 -0300

    test: should try to validate null value

--- test/CPF.test.ts
+++ test/CPF.test.ts
@@ -23,3 +23,6 @@ test("should try to validate an cpf if digits that less than minimum number of c
 });
 
 
+test("should try to validate null value", () => {
+	expect(() => new CPF(null as any)).toThrowError(new Error("Invalid CPF"));
+});
