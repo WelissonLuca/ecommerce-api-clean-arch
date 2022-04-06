15c76cab9fbf156e5f06484ddfbcbd04be34b2bc
Welisson luca <welissonluca17@gmail.com>
2022-04-06 08:02:10 -0300

    test: should try to validate an invalid cpf

--- test/CPF.test.ts
+++ test/CPF.test.ts
@@ -4,3 +4,8 @@ test("should return valid CPF", function () {
   const cpf = new CPF("152.726.480-72");
 	expect(cpf).toBeTruthy();
 });
+test("should try to validate an invalid cpf", function () {
+	 const cpf = new CPF("111.111.111-11");
+	expect(cpf).toBeFalsy();
+});
+
