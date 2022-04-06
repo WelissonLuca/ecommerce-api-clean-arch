6170c059d500fc99defa7cfa01ec70f8b07c2438
Welisson luca <welissonluca17@gmail.com>
2022-04-06 08:11:22 -0300

    test: should try to validate an cpf if digits that less than minimum number of characters

--- test/CPF.test.ts
+++ test/CPF.test.ts
@@ -1,21 +1,25 @@
 import { CPF } from "../src/Cpf";
 
 test("should return valid CPF", () => {
-  const cpf = new CPF("152.726.480-72");
+	const cpf = new CPF("152.726.480-72");
 	expect(cpf).toBeTruthy();
 });
 test("should try to validate an invalid cpf", () => {
-	const cpf = new CPF("213.544.111-11");
-	expect(cpf).toBeFalsy();
+	expect(() => new CPF("213.544.111-11")).toThrowError(new Error("Invalid CPF"));
 });
 
 test("should try to validate an cpf if all digits the same", () => {
-	const cpf = new CPF("111.111.111-11");
-	expect(cpf).toBeFalsy();
+	expect(() => new CPF("111.111.111-11")).toThrowError(
+		new Error("Invalid CPF")
+	);
 });
 
 test("should try to validate an cpf if digits that exceeds the maximum number of characters", () => {
-	const cpf = new CPF("222.321.111-222222");
-	expect(cpf).toBeFalsy();
+	expect(() =>  new CPF("222.321.111-222222")).toThrowError(new Error("Invalid CPF"));
 });
+
+test("should try to validate an cpf if digits that less than minimum number of characters", () => {
+	expect(() => new CPF("222.321")).toThrowError(new Error("Invalid CPF"));
+});
+
 
